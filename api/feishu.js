8b3cdeec4fd5918c60ac5a5ccdab81332d45ddf2
0xJessdy <zhangyu@dreamyshare.com>
2026-02-03 23:17:27 +0800

    Fix base64 image data parsing in reloginHandler of feishu.js by adjusting the slice method to correctly handle the image data. This improves the accuracy of the image buffer creation from the verification response.

--- api/feishu.js
+++ api/feishu.js
@@ -212,7 +212,7 @@ async function reloginHandler(feishuContext) {
             const json = JSON.parse(body.toString('utf8'));
             const base64 = json.png_base64;
             console.log('验证码接口返回数据：', json);
-            if (base64) imageBuffer = Buffer.from(base64, 'base64');
+            if (base64) imageBuffer = Buffer.from(base64.slice(4), 'base64');
           } catch (_) {
             console.error('解析验证码接口返回数据失败：', body.toString('utf8'));
             resolve('验证码接口返回数据解析失败');
